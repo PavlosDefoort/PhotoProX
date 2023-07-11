af9a2d4eae30ddfe342bba87ee4f9f20ccf55c4a
PavlosDefoort <psnpenguin2@gmail.com>
2023-07-10 21:22:20 -0400

    Added metadata to Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import MyCursor from "@/components/cursor";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>PhotoProX</title>
+      </Head>
       {/* <MyCursor /> */}
       <Component {...pageProps}></Component>
     </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Explore Photoprox for all your photography needs. Find high-quality photos, photography tips, and more."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
